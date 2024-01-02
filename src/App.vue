35986b35a7304d4e5843f276575dfb13f385cf9c
DoanVVTrong <trongtk24052003@gmail.com>
2024-01-03 03:23:22 +0700

    FE08. Fix Layout Default --done

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ const default_layout = "default";
 export default {
   computed: {
     layout() {
-      return (this.$route.meta.layout || default_layout) + "-layout";
+      return this.$route.meta.layout + "-layout";
     },
   },
 };
