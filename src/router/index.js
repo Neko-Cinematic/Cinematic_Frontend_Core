e51bcbaf39df9aca9924a1652a5b3fe29ba5ba98
DoanVVTrong <trongtk24052003@gmail.com>
2024-01-03 03:20:06 +0700

    FE08. Rebuild Router & Add Router Link

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,78 @@ import {
     createWebHistory
 } from "vue-router";
 const routes = [{
-    path: "/",
-    component: () => import("../components/Client/Homepage/index.vue"),
-    meta: {
-        layout: "client"
+        path: "/dashboard",
+        component: () => import("../components/Admin/Dashboard/index.vue"),
+    }, {
+        path: "/",
+        component: () => import("../components/Client/Homepage/index.vue"),
+        meta: {
+            layout: "client"
+        },
+    }, {
+        path: "/search",
+        component: () => import("../components/Client/Search/index.vue"),
+        meta: {
+            layout: "client"
+        },
+    }, {
+        path: "/detail",
+        component: () => import("../components/Client/Detail/index.vue"),
+        meta: {
+            layout: "client"
+        },
+    }, {
+        path: "/movie",
+        component: () => import("../components/Client/AllMovie/index.vue"),
+        meta: {
+            layout: "client"
+        },
+    }, {
+        path: "/detail",
+        component: () => import("../components/Client/Detail/index.vue"),
+        meta: {
+            layout: "client"
+        },
     },
-}, ]
+    {
+        path: "/auth",
+        component: () => import("../components/Auth/index.vue"),
+        meta: {
+            layout: "auth"
+        },
+    }, {
+        path: "/watch",
+        component: () => import("../components/Client/Watch/index.vue"),
+        meta: {
+            layout: "client"
+        },
+    },
+    {
+        path: "/detail",
+        component: () => import("../components/Client/Detail/index.vue"),
+        meta: {
+            layout: "client"
+        },
+    }, {
+        path: "/all-movie",
+        component: () => import("../components/Client/AllMovie/index.vue"),
+        meta: {
+            layout: "client"
+        },
+    }, {
+        path: "/detail",
+        component: () => import("../components/Client/Detail/index.vue"),
+        meta: {
+            layout: "client"
+        },
+    }, {
+        path: "/watch",
+        component: () => import("../components/Client/Watch/index.vue"),
+        meta: {
+            layout: "client"
+        },
+    }
+]
 
 const router = createRouter({
     history: createWebHistory(),
